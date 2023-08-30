0282059845e16e487426feb5a114da17ddc59d0d
sb17057 <sbarnes@vermeer.com>
2023-08-30 08:43:59 -0500

    init

---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,14 @@
+namespace GameOfLife;
+
+public class GameOfLife
+{
+    public GameOfLife()
+    {
+        
+    }
+
+    public void Run(int evolutions)
+    {
+        
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var validNumber = false;
+Console.Write("How many evolutions: ");
+while (!validNumber)
+{
+    var evolutionsString = Console.ReadLine();
+    try
+    {
+        var evolutions = int.Parse(evolutionsString!);
+        validNumber = true;
+        Console.WriteLine($"We will evolve {evolutions} times.");
+        var game = new GameOfLife.GameOfLife();
+        game.Run(evolutions);
+    }
+    catch(Exception e)
+    {
+        Console.Write("Please enter a valid number: ");
+    }
+}

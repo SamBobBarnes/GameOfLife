24cd46e8d3bf353649e29ba4eeb19cc6c4749944
sb17057 <sbarnes@vermeer.com>
2023-08-30 10:34:07 -0500

    birth

--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -2,9 +2,9 @@ namespace GameOfLife;
 
 public class GameOfLife
 {
-    private int[,] _board;
-    private int _width;
-    private int _height;
+    protected int[,] _board;
+    protected int _width;
+    protected int _height;
     public GameOfLife(int width = 100, int height = 100)
     {
         _width = width;
@@ -14,7 +14,44 @@ public class GameOfLife
 
     public void Run(int evolutions)
     {
-        
+        throw new NotImplementedException();
+    }
+
+    public void Birth(int x, int y)
+    {
+        _board[x, y] = 1;
+    }
+
+    public bool ChechForBirth(int x, int y)
+    {
+        var left = x - 1 == -1 ? _width - 1 : x-1;
+        var right = x + 1 == _width ? 0 : x+1;
+        var up = y - 1 == -1 ? _height - 1 : y-1;
+        var down = y + 1 == _height ? 0 : y+1;
+
+        var relaventNeighbors = new List<Pair>
+        {
+            new(left, up),
+            new(left, y),
+            new(left, down),
+            new(x, up),
+            new(x, down),
+            new(right, up),
+            new(right, y),
+            new(right, down)
+        };
+
+        return relaventNeighbors.Count(p => _board[p.X, p.Y] == 1) == 3;
+    }
+
+    public void Kill(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CheckForDeath(int x, int y)
+    {
+        throw new NotImplementedException();
     }
 
     public override string ToString()
@@ -31,5 +68,27 @@ public class GameOfLife
         }
 
         return output;
+    }
+}
+
+public class Pair
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Pair(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+public class TestGameOfLife : GameOfLife
+{
+    public TestGameOfLife(int[,] board) : base()
+    {
+        _width = board.GetLength(0);
+        _height = board.GetLength(1);
+        _board = board;
     }
 }
--- UnitTests/GameOfLifeTests.cs
+++ UnitTests/GameOfLifeTests.cs
@@ -1,11 +1,16 @@
 namespace UnitTests;
 
+using Game = GameOfLife.GameOfLife;
+using TestGame = GameOfLife.TestGameOfLife;
+
 public class GameOfLifeTests
 {
+   #region Initialization
+
    [Fact]
    public void GameInitializesCorrectly()
    {
-      var actual = new GameOfLife.GameOfLife();
+      var actual = new Game();
 
       var actualLines = actual.ToString().Split("\n");
       
@@ -14,4 +19,54 @@ public class GameOfLifeTests
       actualLines[0].Should()
          .Be("                                                                                                    ");
    }
+   
+   [Fact]
+   public void GameInitializesCorrectly_9x9()
+   {
+      var actual = new Game(9,9);
+
+      var actualLines = actual.ToString().Split("\n");
+      
+      actualLines.Length.Should().Be(9);
+      actualLines[0].Length.Should().Be(9);
+      actualLines[0].Should()
+         .Be("         ");
+   }
+
+   #endregion
+
+   #region Birth
+
+   [Fact]
+   public void Birth()
+   {
+      var game = new Game(3, 3);
+      game.Birth(0,0);
+
+      var actual = game.ToString();
+
+      var expected = "0  \n   \n   ";
+
+      actual.Should().Be(expected);
+   }
+   
+   [Fact]
+   public void CheckForBirth_ReturnsFalseIfCannotBirth()
+   {
+      var game = new TestGame(new int[3,3]{{0,0,0},{0,0,1},{0,1,0}});
+      var actual = game.ChechForBirth(1,1);
+
+      actual.Should().Be(false);
+   }
+   
+   [Fact]
+   public void CheckForBirth_ReturnsTrueIfCanBirth()
+   {
+      var game = new TestGame(new int[3,3]{{1,0,0},{0,0,1},{0,1,0}});
+      var actual = game.ChechForBirth(1,1);
+
+      actual.Should().Be(true);
+   }
+
+   #endregion
 }
